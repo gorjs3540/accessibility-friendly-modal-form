0f9e313eed063b947c479ef78349b8ce134ee368
sungun <gorjs3540@gmail.com>
2025-09-20 14:40:26 +0900

    feat: useFormDialog 기능 작동까지 구현

---
+++ src/hooks/useFormDialog.tsx
@@ -0,0 +1,53 @@
+import { useOverlay } from "@toss/use-overlay";
+import { useMemo } from "react";
+import Dialog from "../components/Dialog";
+import { Input, Select } from "../components";
+
+export const useFormDialog = () => {
+  const overlay = useOverlay();
+
+  return useMemo(
+    () => ({
+      open: () =>
+        overlay.open(({ close }) => (
+          <FormDialog open={true} onDimmerClick={close} />
+        )),
+      close: () => overlay.close(),
+    }),
+    [overlay],
+  );
+};
+
+const FormDialog = ({
+  open,
+  onDimmerClick,
+}: {
+  open: boolean;
+  onDimmerClick: () => void;
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onDimmerClick={onDimmerClick}
+      header={
+        <Dialog.Header>
+          <h2>신청 폼</h2>
+          <p>이메일과 FE 경력 연차 등 간단한 정보를 입력해주세요.</p>
+        </Dialog.Header>
+      }
+      footer={
+        <Dialog.Footer className="flex justify-end">
+          <button>취소</button>
+          <button>제출하기</button>
+        </Dialog.Footer>
+      }
+    >
+      <div className="flex flex-col p-5 gap-4">
+        <Input label="이름 / 닉네임" />
+        <Input label="이메일" />
+        <Select label="FE 경력 연차" />
+        <Input label="GitHub 링크 (선택)" />
+      </div>
+    </Dialog>
+  );
+};
